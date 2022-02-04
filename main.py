18b1c1d4a79a124f8b893401a67e05bc70544ef4
Min Ji Kim <minjikim.mjk18@gmail.com>
2022-02-03 21:31:28 -0500

    Added a function that takes an integer value and return if response code was successful

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ import socket
 def main():
     print('Hello World!')
 
+def successful_response_test(code):
+    success_codes = [200, 201, 202, 203, 204, 205, 207, 208, 226]
+    if code in success_codes:
+        return True
+    return False
+
+
 if __name__ == '__main__':
     main()
